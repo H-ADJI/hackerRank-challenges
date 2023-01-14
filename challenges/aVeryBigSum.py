cc892fe6a4c495a88ae33c90100f80cd54408f60
H-ADJI <khalil.o.3.lach@gmail.com>
2023-01-14 11:36:19 +0100

    added new challenges solution

---
+++ challenges/aVeryBigSum.py
@@ -0,0 +1,11 @@
+'''
+File: aVeryBigSum.py
+File Created: Saturday, 14th January 2023 11:34:19 am
+Author: KHALIL HADJI 
+-----
+Copyright:  KHALIL HADJI 2023
+'''
+
+
+def aVeryBigSum(ar):
+    return sum(ar)
