1b3ff2a0ce15124f071d2d055c72198795424262
H-ADJI <khalil.o.3.lach@gmail.com>
2023-01-14 11:22:22 +0100

    added command to print all solved challenges + date

--- setup_challenge.py
+++ setup_challenge.py
@@ -1,9 +1,30 @@
 import typer
+import os
+import time
+import re
+from pathlib import Path
+CHALLENGES_PATH = "./challenges"
+FILES_NAME_PATTERN = r".+(?=\.py)"
+cli = typer.Typer()
 
 
-def main(name: str):
-    print(f"Hello {name}")
+@cli.command()
+def all_solved():
+    challenge_files = os.listdir(CHALLENGES_PATH)
+    for file_name in challenge_files:
+        print(re.search(pattern=FILES_NAME_PATTERN, string=file_name).group())
+        print(time.ctime(os.path.getctime(Path(CHALLENGES_PATH).joinpath(file_name))))
+
+
+@cli.command()
+def last_solved():
+    pass
+
+
+@cli.command()
+def solved_count():
+    pass
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    cli()
