18e06cb7136c3058c433338cb3a7ae388a0733fd
H-ADJI <khalil.o.3.lach@gmail.com>
2023-01-14 19:09:16 +0100

    added other cli commands

--- setup_challenge.py
+++ setup_challenge.py
@@ -1,30 +1,41 @@
 import typer
 import os
 import time
+from utils import get_challenge_name, get_file_creation_date,challenge_files
 import re
 from pathlib import Path
-CHALLENGES_PATH = "./challenges"
-FILES_NAME_PATTERN = r".+(?=\.py)"
+from rich import print as rprint
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
 cli = typer.Typer()
 
 
 @cli.command()
 def all_solved():
-    challenge_files = os.listdir(CHALLENGES_PATH)
     for file_name in challenge_files:
-        print(re.search(pattern=FILES_NAME_PATTERN, string=file_name).group())
-        print(time.ctime(os.path.getctime(Path(CHALLENGES_PATH).joinpath(file_name))))
+        challenges_table.add_row(get_challenge_name(
+            file_name=file_name), time.ctime(get_file_creation_date(file_name=file_name)))
+    console.print(challenges_table)
 
 
 @cli.command()
 def last_solved():
-    pass
+    last = [0, None]
+    for file_name in challenge_files:
+        creation_time = get_file_creation_date(file_name=file_name)
+        if last[0] <= creation_time:
+            last = [creation_time,
+                    get_challenge_name(file_name=file_name)]
+    rprint(last)
 
 
 @cli.command()
 def solved_count():
-    pass
+    rprint(f"You solved {len(challenge_files)} problem so far")
 
 
 if __name__ == "__main__":
+    challenges_table = Table("challenge name", "solved on")
     cli()
