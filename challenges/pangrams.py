02f7f11f4708615e160b9bdc27d38d5cd396cfd2
H-ADJI <khalil.o.3.lach@gmail.com>
2023-01-25 21:49:15 +0100

    added new challenge

--- challenges/pangrams.py
+++ challenges/pangrams.py
@@ -3,7 +3,13 @@ File: pangrams.py
 -----
 Author: KHALIL HADJI 
 '''
+import string
 
 
-def pangrams(*args,**kwargs):
-    pass
+def pangrams(s):
+    used_alphabet = set(s.lower())
+    print(used_alphabet)
+    alphabet = set(string.ascii_lowercase)
+    if alphabet.intersection(used_alphabet) == alphabet:
+        return "pangram"
+    return "not pangram"
