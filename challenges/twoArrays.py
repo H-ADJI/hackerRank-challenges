172c860929e99378465cad9773af83f6f5054596
H-ADJI <khalil.o.3.lach@gmail.com>
2023-01-26 19:51:30 +0100

    added new challenge

---
+++ challenges/twoArrays.py
@@ -0,0 +1,10 @@
+'''
+File: twoArrays.py
+-----
+Author: KHALIL HADJI 
+'''
+
+def twoArrays(k, A, B):
+    A.sort(reverse=True)
+    B.sort()
+    return "YES" if all([t[0] + t[1] >= k for t in zip(A, B)]) else "NO"
